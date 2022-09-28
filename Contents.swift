1b25c540eb5fc4e23f199cdcba11e59ac7e99473
Devesh <devesh_hanumante@outlook.com>
2022-09-28 11:35:41 +0200

    update

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,48 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Operadores
+
+//Operadores de asignacion
+
+let a = 5
+var b = 10
+
+b = a
+
+//Operadores Aritmeticos
+
+print(1 + 2) //suma
+print(1 - 2) //Resta
+print(2 * 4) //Multiplicacion
+print(4 / 2) //division
+print(8 % 3) //modulo
+
+//Operador negativo
+
+var c = -b
+
+//Operador de composición
+
+b += 10 // b = b + 10
+
+//Operadores Comparación
+
+//Igual
+print(1 == 1)
+
+//Distinto
+print(1 != 1)
+
+//Mayor que
+print(2 > 1)
+
+//Menor que
+print(1 < 2)
+
+//Mayor o igual que
+
+print(2 >= 1)
+
+//Menor o igual que
+
+print(1 <= 2)
